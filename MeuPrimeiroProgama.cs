31e9e937379e56c8e354c27dc6305db037a16c27
Aluno <aluno@edu.senac.br>
2023-12-19 20:10:13 -0300

    total

---
+++ MeuPrimeiroProgama.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testesoftware
+{
+    public class MeuPrimeiroProgama
+    {
+
+        public void MostrarMensagem(string mensagem){
+            Console.WriteLine(mensagem);
+        }
+        /*
+        
+        Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite o primeiro número");
+string? numero1 = Console.ReadLine();
+Console.WriteLine("Digite o segundo número");
+string? numero2 = Console.ReadLine();
+int soma = int.Parse(numero1)+int.Parse(numero2);
+Console.WriteLine("A soma é: " + soma );
+if(soma > 10){
+  Console.WriteLine("A soma é maior que 10");
+}else if(soma == 10){
+  Console.WriteLine("A soma é igual a 10");
+}else{
+    Console.WriteLine("A soma é menor que 10");
+}   
+
+        
+    }*/
+
+}
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita a quantidade de produtos em estoque
+        Console.Write("Informe a quantidade de produtos em estoque: ");
+
+        // Tenta converter a entrada do usuário para um número inteiro
+        if (int.TryParse(Console.ReadLine(), out int quantidadeEmEstoque))
+        {
+            // Solicita o preço unitário dos produtos em estoque
+            Console.Write("Informe o preço unitário dos produtos em estoque: ");
+
+            // Tenta converter a entrada do usuário para um número decimal (preço unitário)
+            if (decimal.TryParse(Console.ReadLine(), out decimal precoUnitario))
+            {
+                // Calcula o valor total em estoque
+                decimal valorTotalEmEstoque = quantidadeEmEstoque * precoUnitario;
+
+                // Exibe a quantidade de produtos em estoque e o valor total
+                Console.WriteLine($"A quantidade de produtos em estoque é: {quantidadeEmEstoque}");
+                Console.WriteLine($"O preço unitário dos produtos em estoque é: {precoUnitario:C}");
+                Console.WriteLine($"O valor total em estoque é: {valorTotalEmEstoque:C}");
+            }
+            else
+            {
+                // Exibe uma mensagem de erro se a entrada não for um número decimal válido
+                Console.WriteLine("Por favor, insira um preço unitário válido.");
+            }
+        }
+        else
+        {
+            // Exibe uma mensagem de erro se a entrada da quantidade não for um número inteiro válido
+            Console.WriteLine("Por favor, insira uma quantidade válida.");
+        }
+    }
+}
